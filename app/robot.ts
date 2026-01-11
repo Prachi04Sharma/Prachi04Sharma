0f9942a0f65bc37c9f9f2b6a0bcfd7699ffdd2fa
Prachi Sharma <122223354+Prachi04Sharma@users.noreply.github.com>
2026-01-12 03:59:33 +0530

    wip-1-add-robot-llms-sitemap

---
+++ app/robot.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/api/', '/_next/'],
+    },
+    sitemap: 'https://prachi2k5sharma.vercel.app/sitemap.xml',
+  }
+}
